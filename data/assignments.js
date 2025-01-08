63d41679652b736f0ec164375d73aa74ed6efd29
planit <planit@tanjeffreyz.github.io>
2025-01-08 19:32:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -6,6 +6,13 @@ assignments = {
       "dueDate": "2025-01-08T10:00:00",
       "link": "https://www.gradescope.com/courses/933270/assignments/5540470/submissions/299081045",
       "submitted": true
+    },
+    {
+      "title": "ex1 - Exercise 1",
+      "course": "CSE 333",
+      "dueDate": "2025-01-10T10:00:00",
+      "link": "https://www.gradescope.com/courses/933270",
+      "submitted": false
     }
   ],
   "CSE 369": []
