335fdcee267751712f368c648facfbee76e6014d
planit <planit@tanjeffreyz.github.io>
2025-02-03 22:35:15 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -113,6 +113,13 @@ assignments = {
       "dueDate": "2025-02-05T14:30:00",
       "link": "https://www.gradescope.com/courses/942142",
       "submitted": false
+    },
+    {
+      "title": "Lab 3 Demo (No Submit)",
+      "course": "CSE 369",
+      "dueDate": "2025-01-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/942142",
+      "submitted": false
     }
   ]
 };
