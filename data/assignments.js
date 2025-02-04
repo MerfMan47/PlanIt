5b126ec984daba9d05a81bf664755176fc3318fc
planit <planit@tanjeffreyz.github.io>
2025-02-04 01:54:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -83,6 +83,13 @@ assignments = {
       "dueDate": "2025-02-05T10:00:00",
       "link": "https://www.gradescope.com/courses/933270",
       "submitted": false
+    },
+    {
+      "title": "hw1 bonus",
+      "course": "CSE 333",
+      "dueDate": "2025-01-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/933270",
+      "submitted": false
     }
   ],
   "CSE 369": [
