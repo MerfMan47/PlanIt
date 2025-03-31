0e05e0d2ca666d43facbdd46946b25c2797f701f
planit <planit@tanjeffreyz.github.io>
2025-03-31 19:32:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
   "CSE 312 - 25sp": [],
-  "CSE 332": []
+  "CSE 332": [
+    {
+      "title": "EX00: (Stacks & Queues) Programming Portion ",
+      "course": "CSE 332",
+      "dueDate": "2025-04-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/1011407",
+      "submitted": false
+    }
+  ]
 };
