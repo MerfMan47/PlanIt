6fdca37b96c0d7b502b93ff9498bbc6396baa37d
planit <planit@tanjeffreyz.github.io>
2025-04-01 02:22:25 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,8 +1,30 @@
 assignments = {
-  "CSE 312 - 25sp": [],
+  "CSE 312 - 25sp": [
+    {
+      "title": "Concept Check 01: Sum & Product Rules;  Permutation",
+      "course": "CSE 312 - 25sp",
+      "dueDate": "2025-04-04T13:00:00",
+      "link": "https://www.gradescope.com/courses/1012980/assignments/6010040/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Concept Check 00: Survey",
+      "course": "CSE 312 - 25sp",
+      "dueDate": "2025-04-04T13:00:00",
+      "link": "https://www.gradescope.com/courses/1012980/assignments/6010053/submissions/new",
+      "submitted": false
+    }
+  ],
   "CSE 332": [
     {
       "title": "EX00: (Stacks & Queues) Programming Portion ",
+      "course": "CSE 332",
+      "dueDate": "2025-04-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/1011407",
+      "submitted": false
+    },
+    {
+      "title": "EX00: Benchmarking",
       "course": "CSE 332",
       "dueDate": "2025-04-07T23:59:00",
       "link": "https://www.gradescope.com/courses/1011407",
